c5c8b317b89466004574170c05c1ad7fe357d3f9
Kea Moseki <neoentlemoseki@gmail.com>
2026-02-23 16:09:37 +0200

    Initial Selenium project

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void SetUp() {
+        driver = DriverFactory.initDriver();
+        driver.get("https://google.com");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/DriverFactory.java
@@ -0,0 +1,11 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+
+    public static WebDriver initDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+}
